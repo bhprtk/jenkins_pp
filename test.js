b08a799e397a552875c1b49d4578a539be0d2de2
pratik <lazy.monk.13@gmail.com>
2018-08-18 15:09:30 -0700

    test 42

--- test.js
+++ test.js
@@ -4,7 +4,12 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function(){
       sum = 1;
         for(let i = 0; i < 99999999999999999999; i++) {
-          sum = sum * i
+          for(let j = 1; j < 9999999999999999; j++ ) {
+            for(let k = 2; k < 99999999999999; k++) {
+              sum = sum * i * j * k
+
+            }
+          }
         }
         assert.equal(-1, [1,2,3].indexOf(4))
     })
