2f03ea79ffbd7c51fc88360c99b21622ff819501
pratik <lazy.monk.13@gmail.com>
2018-08-19 14:58:47 -0700

    test 74

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ const assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('test 1', function(done){
+        let product = 1
         for(let i = 1; i < 9999999999999; i++){ 
-
+          product *= i
         }
         assert.equal(-1, [1,2,3].indexOf(4))
         done()
