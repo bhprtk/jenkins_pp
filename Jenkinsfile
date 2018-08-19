2e96da28afe400cd669fcd9940306a3491a43977
pratik <lazy.monk.13@gmail.com>
2018-08-19 14:54:23 -0700

    test 72

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,20 +1,12 @@
-// pipeline {
-//     agent any
-//     stages {
-//         stage('Test') {
-//             steps {
-//                 runTests()
-//             }
-//         }
-//     }
-// }
-
-def runTests(args) {
-    sh 'npm test'
-}
-
-node {
-    stage('Test') {
-        runTests()
+pipeline {
+    agent any
+    stages {
+        stage('Test') {
+            steps {
+                sh 'npm test'
+            }
+        }
     }
 }
+
+
--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 const assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(done){
+    it('test 1', function(done){
+        for(let i = 1; i < 9999999999; i++){ 
+          
+        }
+        assert.equal(-1, [1,2,3].indexOf(4))
+        done()
+    })
+    it('test 2', function(done){
         
         assert.equal(-1, [1,2,3].indexOf(4))
         done()
