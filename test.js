df25f94780cec36ad33c156b740f9ac55e1fa7fd
pratik <lazy.monk.13@gmail.com>
2018-08-18 22:16:04 -0700

    test 56

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
+    it('should return -1 when the value is not present', function(done){
       let sum = 1;
       for(let i = 0; i < 999999999; i++) {
         sum = sum * i
       }
         
         assert.equal(-1, [1,2,3].indexOf(4))
-    })
+    }).finally(done)
   })
 })
