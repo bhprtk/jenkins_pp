b46d1e42c35f5113f446b9c4ca99429925c61f00
pratik <lazy.monk.13@gmail.com>
2018-08-25 13:24:11 -0700

    test 92

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('test 1', function(done){
       let product = 1
-      while(true) {
+      for(let i = 0; i < 1000000; i++) {
         assert.equal(-1, [1,2,3].indexOf(4))
-            done()
+        done()
       }
     })
     it('test 2', function(done){
