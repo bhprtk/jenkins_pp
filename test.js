75ade434aea2bf123a1da44dcbfe76b474762f84
pratik <lazy.monk.13@gmail.com>
2018-08-25 12:21:44 -0700

    test 87

--- test.js
+++ test.js
@@ -3,15 +3,11 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('test 1', function(done){
       let product = 1
-      for(let i = 1; i < 9999999999999; i++){ 
-        for(let j = 1; j < 99999999999; j++) {
-          for(let k = 1; k < 9999999999; k++){ 
-            product *= i
-          }
-        }
-      }
+      while(true){
         assert.equal(-1, [1,2,3].indexOf(4))
         done()
+
+      }
     })
     it('test 2', function(done){
         assert.equal(-1, [1,2,3].indexOf(4))
