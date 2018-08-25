384a9e632cd4d759c222155639db04df88d3812a
pratik <lazy.monk.13@gmail.com>
2018-08-25 12:37:48 -0700

    test 89

--- test.js
+++ test.js
@@ -2,22 +2,19 @@ const assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('test 1', function(done){
-      let product = 1, limit = 10000000000000, primes = 0
-      for (num = 1; num <= limit; num++) { 
-        let i = 2; 
-        while(i <= num) { 
-            if(num % i == 0)
-                break;
-            i++; 
+      let product = 1
+      for(let i = 0; i < 1000000000000000; i++) {
+        assert.equal(-1, [1,2,3].indexOf(4))
+        done()
+        for(let j = 0; j < 10000000000000000; j++) {
+          assert.equal(-1, [1,2,3].indexOf(4))
+          done()
+          for(let k = 0; k < 100000000000000; k++) {
+            assert.equal(-1, [1,2,3].indexOf(4))
+            done()
+          }
         }
-        if(i == num)
-            primes++;
       }
-      // while(true){
-      //   assert.equal(-1, [1,2,3].indexOf(4))
-      //   done()
-
-      // }
     })
     it('test 2', function(done){
         assert.equal(-1, [1,2,3].indexOf(4))
