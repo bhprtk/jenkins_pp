a692cfc72e85571cfc53b311058ffabe34304c8b
pratik <lazy.monk.13@gmail.com>
2018-08-25 19:35:04 -0700

    test 105

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ const assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('test 1', function(done){
-      while(true) {
-        assert.equal(-1, [1,2,3].indexOf(4))
-      }
+      // while(true) {
+      // }
+      assert.equal(-1, [1,2,3].indexOf(4))
       done()
     })
   })
